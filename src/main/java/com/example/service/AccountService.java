57858a5671fffdc67588951a6b4cbbe913c069ec
Nathan Allers <nateallers@gmail.com>
2024-08-23 00:43:55 +0000

    Finish adding comments

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -19,11 +19,23 @@ public class AccountService
         this.accountRepository = accountRepository;
     }
 
+    /**
+     * Uses the AccountRepository to check if an account exists based on its username
+     * @param username The username of the account to check
+     * @return Boolean indicating whether the account exists
+     */
     public boolean accountExistsByUsername(String username)
     {
         return accountRepository.existsByUsername(username);
     }
 
+    /**
+     * Uses the AccountRepository to save an account if it meets these requirements:
+     * * the username is not blank
+     * * the password is at least 4 characters long
+     * @param account The account to save
+     * @return Optional<Account> of the account saved or empty if unsuccessful
+     */
     public Optional<Account> saveAccount(Account account)
     {
         Optional<Account> result = Optional.empty();
@@ -34,6 +46,11 @@ public class AccountService
         return result;
     }
 
+    /**
+     * Uses the AccountRepository to authenticate an account
+     * @param account The account to authenticate
+     * @return Optional<Account> of the authenticated account or empty if unsuccessful
+     */
     public Optional<Account> getAuthenticatedAccount(Account account)
     {
         Optional<Account> authenticatedAccount = Optional.empty();
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -23,11 +23,20 @@ public class MessageService
         this.accountRepository = accountRepository;
     }
 
+    /**
+     * Uses the MessageRepository to get a list of all existing messages
+     * @return The list of messages that currently exist
+     */
     public List<Message> getAllMessages()
     {
         return messageRepository.findAll();
     }
 
+    /**
+     * Uses the MessageRepository to save a message if the message text is valid and the account posting it exists
+     * @param message The message to save
+     * @return Optional<Message> of the saved message or empty if unsuccessful
+     */
     public Optional<Message> saveMessage(Message message)
     {
         Optional<Message> result = Optional.empty();
@@ -40,11 +49,21 @@ public class MessageService
         return result;
     }
 
+    /**
+     * Uses the MessageRepository to get a message based on the message's ID
+     * @param id The ID of the message to get
+     * @return Optional<Message> of the message to get or empty if unsuccessful
+     */
     public Optional<Message> getMessageByID(int id)
     {
         return messageRepository.findById(id);
     }
 
+    /**
+     * Uses the MessageRepository to delete a message based on the message's ID
+     * @param id The ID of the message to delete
+     * @return Boolean indicating if deletion was successful
+     */
     public boolean deleteMessageByID(int id)
     {
         boolean deleted = false;
@@ -56,11 +75,22 @@ public class MessageService
         return deleted;
     }
 
+    /**
+     * Uses the MessageRepository to get a list of all messages posted by an acount with a specified ID
+     * @param postedBy The ID of the account that posted the messages to get
+     * @return List of messages posted by the account with the specified ID
+     */
     public List<Message> findMessagesByPostedBy(int postedBy)
     {
         return messageRepository.findByPostedBy(postedBy);
     }
 
+    /**
+     * Uses the MessageRepository to update a message based on the message's ID
+     * @param message Message with updated message text
+     * @param id The ID of the message to update
+     * @return Boolean indicating whether the update was successful
+     */
     public boolean updateMessage(Message message, int id)
     {
         boolean updated = false;
@@ -76,6 +106,13 @@ public class MessageService
         return updated;
     }
 
+    /**
+     * Validates message text based on these requirements:
+     * * the text is not blank
+     * * the text is less than 255 characters long
+     * @param message_text The message text to validate
+     * @return Boolean indicating whether the text is valid
+     */
     public boolean isValidMessageText(String message_text)
     {
         return !message_text.isBlank() && message_text.length() < 255;
